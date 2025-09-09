aa9a37cd7aa006a774bb447bd9babf9b5d1bba3f
arnavkumar2k5 <144643023+arnavkumar2k5@users.noreply.github.com>
2025-09-09 10:18:23 +0530

    msc form added

--- src/components/Hero-Section/index.jsx
+++ src/components/Hero-Section/index.jsx
@@ -18,13 +18,13 @@ export function HeroSection() {
           <div className="absolute inset-0 bg-gradient-to-r from-white/20 via-blue-300/30 to-cyan-300/20 blur-3xl -z-10 animate-pulse"></div>
         </h1>
         <div className="w-32 h-1 bg-gradient-to-r from-transparent via-white to-transparent mx-auto opacity-70 animate-fade-in animation-delay-2000"></div>
-          {/* <Link to="/joinmsc">
+          <Link to="/joinmsc">
             <button
               className="mt-10 px-4 py-1.5 rounded-full font-semibold text-sm md:text-base tracking-wide bg-gradient-to-br from-white via-blue-100 to-cyan-200 text-[#0078D4] border border-[#0078D4] shadow-sm hover:bg-blue-50 hover:text-blue-500 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-200 cursor-pointer"
             >
               MSC Recruitment is Live! Apply Now
             </button>
-          </Link> */}
+          </Link>
       </div>
       <AmoebaComponent/>
       <CanvasCursor/>
